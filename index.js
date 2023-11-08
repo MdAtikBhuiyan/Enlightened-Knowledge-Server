f546076f31eec58d7642350015168f106ebbfc2c
MdAtikBhuiyan <atik15-2642@diu.edu.bd>
2023-11-08 23:17:13 +0600

    vercel-api-problem-solved

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // await client.connect();
 
         const allBookCollection = client.db('libraryBooks').collection('allBooks');
         const borrowedBookCollection = client.db('libraryBooks').collection('borrowedBooks');
@@ -133,7 +133,7 @@ async function run() {
             res.send(result)
         })
 
-        
+
         // // books by id array 
         // app.post('/booksById', async (req, res) => {
         //     const ids = req.body;
@@ -150,7 +150,7 @@ async function run() {
 
 
         // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
+        // await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
 
     } finally {
